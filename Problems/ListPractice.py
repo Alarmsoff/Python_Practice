7a18e638856f8e187ea66b1b441218bed0dbfc12
Alarmsoff <ray1196592383@outlook.com>
2025-03-20 00:48:58 -0400

    add some question in list_practice

---
+++ Problems/ListPractice.py
@@ -0,0 +1,48 @@
+# this file will contain some list and loop quesion from easy to hard
+
+
+import random
+"""
+Question 1
+Please write a function called filter_even()
+Input: a list of int
+Return: a list of int which only contains even int
+
+Example:
+a = [1,2,3,4,5,6]
+print(filter_even(a))
+>>> [2,4,6]
+"""
+def filter_even(lst):
+    # Please fill and edit your code here
+    return 0
+
+
+"""
+Question 2
+Please write function called flat()
+Input: a nested list
+Return: a list contain all the element in the input list
+
+Example:
+print(flatten([1, [2, 3], [4, [5, 6]], 7]))
+>>> [1, 2, 3, 4, 5, 6, 7]
+"""
+def flat(n_lst):
+    # Please fill and edit your code here
+    return 0
+
+"""
+Question 3
+Please write a function called square_prime()
+Input: a list of int
+Return: a list of int which contains the prime num's square
+!Hint!: You can write a helper funtion to check if the num is prime num
+
+Example:
+print(square_primes([2, 3, 4, 5, 6, 7, 8, 9, 10]))
+>>> [4, 9, 25, 49]
+"""
+def square_prime():
+    # Please fill and edit your code here
+    return 0
